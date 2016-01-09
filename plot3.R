11caf94ee03a745a9a8cdfc630a66726fe08b5f8
enisbin <nishchay_iift@yahoo.com>
2016-01-09 19:16:05 +0100

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,15 @@
+Sys.setlocale("LC_TIME","English_United_States")
+datasourcefile<-"C:\\Users\\bindalni\\Documents\\household_power_consumption.txt"
+maindata<-read.table(datasourcefile,header=TRUE,sep=";",stringsAsFactors = FALSE)
+dates<-maindata[maindata$Date %in% c("1/2/2007","2/2/2007"),]
+strptimefunction<-strptime(paste(dates$Date,dates$Time,sep=""),"%d/%m/%Y %H:%M:%S")
+power_data<-as.numeric(dates$Global_active_power)
+meter1<-as.numeric(dates$Sub_metering_1)
+meter2<-as.numeric(dates$Sub_metering_2)
+meter3<-as.numeric(dates$Sub_metering_3)
+png("plot3.png",width=480,height=480)
+plot(strptimefunction, meter1, type="l", ylab="Energy Submetering", xlab="")
+lines(strptimefunction, meter2, type="l", col="red")
+lines(strptimefunction, meter3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
